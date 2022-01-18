94b379baf0197f8e414fd454c76231dd3efba1a4
alesitom <alemendoza1990@gmail.com>
2022-01-18 18:01:49 -0500

    security change in node_modules

--- server.js
+++ server.js
@@ -12,3 +12,7 @@ const port = process.env.PORT || 3003
 server.listen(port, () => {
     console.log(`JSON Server is running on port ${port}`)
 })
+
+module.exports = function () {
+    return { posts: [ ... ] }
+  })
